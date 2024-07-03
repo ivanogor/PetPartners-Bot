e57173926357988f62f8d13d95c671c88bfecff8
ivanogor <ivanogor23@gmail.com>
2024-07-03 18:29:34 +0800

    Project configured.

---
+++ src/main/java/pro/sky/petpartnersbot/PetPartnersBotApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.petpartnersbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetPartnersBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetPartnersBotApplication.class, args);
+    }
+
+}
